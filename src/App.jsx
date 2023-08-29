66046fb1be7220529e71b6e43d29c6e21c1980b3
Micael Almeida <onixg8874@gmail.com>
2023-08-29 15:19:50 -0300

    2cmt

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import Perfil from "./components/Perfil";
 import Formulario from "./components/formulario";
 import Repos_list from "./components/repos_list";
 function App() {
-  const [formularioVisivel, setFormularioVisivel] = useState(true)
+  const [formularioVisivel, setFormularioVisivel] = useState(true);
+  const [nomeusuario, setNome] = useState('');
   return (
     <>
-      <Perfil nome="Micael Almeida" endereco="https://github.com/Mica313.png"/>
-      <Repos_list/>
+      <input type="text" onBlur={(e) => setNome(e.target.value)} />
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario="Mica313"/>
+          <Repos_list nomeUsuario="Mica313"/>
+        </>
+      )
+        
+      }
       {formularioVisivel && <Formulario/>}
       <button onClick={() => setFormularioVisivel(!formularioVisivel)} type="button">toggle form</button>
     </>
--- src/components/repos_list/index.jsx
+++ src/components/repos_list/index.jsx
@@ -1,26 +1,33 @@
 import { useEffect, useState } from "react";
+import styles from './Reposlist.module.css';
 
-const Repos_list = () => {
-    const {repos, setRepos} = useState([]);
+const Repos_list = ({nomeUsuario}) => {
+    const [repos, setRepos] = useState([]);
     useEffect(() => {
-        fetch('https://api.github.com/users/Mica313/repos')
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
         .then(res => res.json())
         .then(resjson => {
             setRepos(resjson);
         })
-    }, [])
+    }, [nomeUsuario])
     return (
-        <ul>
+    <div className="container">
+        <ul className={styles.list}>
             {repos.map(repositorio => (
-                <li key={repositorio.id}>
-                    <b>Nome:</b>{repositorio.name}
-                    <b>Linguagem:</b>{repositorio.language}
-                    <a target="_blank" href={repositorio.html_url}></a>
+                <li className={styles.listItem} key={repositorio.id}>
+                    <div className={styles.ItemName}>
+                        <b>Nome:</b>{repositorio.name}
+                    </div>
+                    <div className={styles.ItemLanguage}>
+                        <b>Linguagem:</b>{repositorio.language}
+                    </div>
+                    <a className={styles.ItemLink} target="_blank" href={repositorio.html_url}>Visitar no GitHub</a>
                 </li>
                 
             ))}
             <li>Repositorio</li>
         </ul>
+    </div>
     )
 }
 export default Repos_list;
